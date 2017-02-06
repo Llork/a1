a5abb20e6670ab5e1c2c63d039c3ce9c02331186
David Kroll <curtain333-code@yahoo.com>
2017-02-06 18:55:34 -0500

    Added index.php file which displays 'under construction' message.

---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <title>David Kroll - DWA15 Assignment 1</title>
+  <link rel="stylesheet" href="/css/styles.css">
+</head>
+
+<body>
+  <p>under construction</p>
+</body>
+
+</html>
