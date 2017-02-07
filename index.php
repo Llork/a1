ecace2edab3122a68a9b48a9b59c7e31159ff63d
David Kroll <curtain333-code@yahoo.com>
2017-02-06 20:36:25 -0500

    Added stylesheet, photo, and headings

--- index.php
+++ index.php
@@ -2,13 +2,29 @@
 <html lang="en">
 
 <head>
-  <meta charset="utf-8">
-  <title>David Kroll - DWA15 Assignment 1</title>
-  <link rel="stylesheet" href="/css/styles.css">
+	<meta charset="utf-8">
+	<title>David Kroll - DWA15 Assignment 1</title>
+	<link rel="stylesheet" href="/css/styles.css">
 </head>
 
 <body>
-  <p>under construction</p>
+	
+	<div class="container">
+    
+    	<h1>David Kroll</h1>
+        
+        	<img src="images/david_kroll.jpg" style="width:25%;min-width:300px;height:auto;" />
+        
+        <h2>About Me</h2>
+        
+  			<p>under construction</p>
+            
+		<h2>Random Quote</h2>
+        
+        	<p>quote "chosen" dynamically by PHP goes here</p>
+                 
+  	</div>  
+  
 </body>
 
 </html>
