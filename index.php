e3e98e107bc496353cca3484670315cf5913f6e2
David Kroll <curtain333-code@yahoo.com>
2017-02-07 20:34:03 -0500

    Added PHP to display random quotation.

--- index.php
+++ index.php
@@ -21,7 +21,15 @@
             
 		<h2>Random Quote</h2>
         
-        	<p>quote "chosen" dynamically by PHP goes here</p>
+        	<p><?php
+			
+			$cool_quotes = array('"Reality is merely an illusion, albeit a very persistent one."<br>Albert Einstein', '"You must be the change you wish to see in the world."<br>Mahatma Gandhi', '"I\'ve learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel."<br>Maya Angelou');
+			
+			$random_number = rand(0,2);
+			
+			echo($cool_quotes[$random_number]);
+			
+			?></p>
                  
   	</div>  
   
